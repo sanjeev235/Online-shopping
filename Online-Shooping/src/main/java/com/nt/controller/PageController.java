dfe964027c057b11c8d04b867b3f6ef05eee95e7
sanjeev singh <sanjeevsinghbsc@gmail.com>
2018-10-26 23:00:12 +0530

    success

---
+++ Online-Shooping/src/main/java/com/nt/controller/PageController.java
@@ -0,0 +1,24 @@
+package com.nt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value={"/","/home","/index"})
+	public ModelAndView index()
+	{
+		
+		ModelAndView mv=new ModelAndView("page");
+		
+		mv.addObject("msg","welcome to Spring MVC web App");
+		
+		return mv;
+		
+		
+	}
+	
+
+}
