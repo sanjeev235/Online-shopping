3e5a3eef509a3b16040cc5e3c7d6e3024c761315
stripathi <stripathi@INPLSTRIPATHI.trekbikes.net>
2018-11-25 11:53:05 +0530

    success

--- Online-Shooping/src/main/java/com/nt/controller/PageController.java
+++ Online-Shooping/src/main/java/com/nt/controller/PageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-	
+	//mapping
 	@RequestMapping(value={"/","/home","/index"})
 	public ModelAndView index()
 	{
